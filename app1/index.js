32a01439e4e75e176d8f0e20ae9c7350c8658654
Jesse Niininen <jesseniininen@gmail.com>
2021-06-12 15:24:46 +0300

    Exercise 1.07

--- app1/index.js
+++ app1/index.js
@@ -1,9 +1,23 @@
 const crypto = require('crypto')
+const express = require('express')
 
 const hash = crypto.createHash('sha1')
     .update(Buffer.from(Math.random().toString()))
     .digest('hex')
 
+const state = () => (new Date()).toISOString() + ' ' + hash
+
 setInterval(() => {
-    console.log((new Date()).toISOString() + ' ' + hash)
+    console.log(state())
 }, 5000)
+
+
+const app = express()
+
+app.get('*', (req, res) => {
+    res.end(state())
+})
+
+app.listen(3000, () => {
+    // console.log('Server started in port 3000')
+})
