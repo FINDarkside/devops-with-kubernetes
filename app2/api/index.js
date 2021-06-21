5c8f7939fecc94b069ea3d9339b891dfa50ee826
Jesse Niininen <jesseniininen@gmail.com>
2021-06-21 23:19:28 +0300

    Exercise 2.08

--- app2/api/index.js
+++ app2/api/index.js
@@ -1,28 +1,52 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const { Pool } = require('pg')
+const promisfy = require('util').promisify
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'postgres-svc',
+    database: 'postgres',
+    password: process.env.POSTGRES_PASSWORD,
+    port: 5432,
+})
+pool.query = promisfy(pool.query)
 
 const app = express()
 app.use(bodyParser.json());
 app.use(cors())
 
-const todos = []
-let id = 0
 
-app.post('/todos', (req, res) => {
+app.post('/todos', async (req, res) => {
     const todo = req.body
-    todo.id = id++
 
     if (typeof todo.text !== 'string' || todo.text.length > 140)
         res.status(400).end()
-    todos.push(todo)
+    await pool.query('INSERT INTO todos (text) VALUES ($1)', [todo.text])
     res.end()
 })
 
-app.get('/todos', (req, res) => {
-    res.json(todos)
+app.get('/todos', async (req, res) => {
+    const todos = await pool.query('SELECT * FROM todos')
+    res.json(todos.rows)
 })
 
-app.listen(3001, () => {
-    console.log('Server started in port 3001')
+
+async function initApp() {
+    const res = await pool.query(`
+        CREATE TABLE IF NOT EXISTS todos (
+            id SERIAL PRIMARY KEY,
+            text VARCHAR
+        );
+    `)
+
+    app.listen(3001, () => {
+        console.log('Server started in port 3001')
+    })
+}
+
+initApp().catch(err => {
+    console.error(err)
+    process.exit(1)
 })
