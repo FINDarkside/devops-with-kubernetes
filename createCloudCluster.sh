61c1518a45ee1d501118b3de66bf0d7b1a599527
Jesse Niininen <jesseniininen@gmail.com>
2021-06-22 21:01:34 +0300

    Add deleteEnvironment action

--- createCloudCluster.sh
+++ createCloudCluster.sh
@@ -1,5 +1,3 @@
 gcloud container clusters create dwk-cluster --zone=europe-north1-b
 gcloud container clusters get-credentials dwk-cluster --zone=europe-north1-b
-kubectl create namespace main-app
-kubectl create namespace project
 kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.12.1/controller.yaml
