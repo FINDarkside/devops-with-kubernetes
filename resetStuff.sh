63cacf337eee672ccdc835dad54fe61a444e6fd2
Jesse Niininen <jesseniininen@gmail.com>
2021-06-28 21:16:29 +0300

    Change deploy path

--- resetStuff.sh
+++ resetStuff.sh
@@ -1,3 +1,7 @@
 k3d cluster delete
 ./create_cluster.sh
+git add ./app2/manifests/pg-sealedsecret.yaml
+git add ./app2/manifests/tg-sealedsecret.yaml
+git commit -m "re-seal secrets"
+git push
 ./bootstrap_flux.sh
